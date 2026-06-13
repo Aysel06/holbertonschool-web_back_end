76e96a2ca201b00539f5f10632ae210ee1a298a5
Aysel06 <mammedovaaysel0@gmail.com>
2026-06-13 12:05:39 +0000

    0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,5 +1,9 @@
 #!/bin/env python3
+
 def index_range(page , page_size):
-    start = (page -1)*page_size
-    end = start + page_size
-    return (start , end)
+"""Returns a tuple of size two containing a start index and an end index
+corresponding to the range of indexes to return in a list for those
+particular pagination parameters"""
+    start_index = (page -1)*page_size
+    end_index = start_index + page_size
+    return (start_index , end_index)
