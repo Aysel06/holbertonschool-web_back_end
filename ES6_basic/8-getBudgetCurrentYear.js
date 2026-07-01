ef8e0a9f6032972337ca4546856be3c508e11706
Aysel06 <mammedovaaysel0@gmail.comg>
2026-07-01 16:50:56 +0000

    8

---
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,16 @@
+function getCurrentYear() {
+    const date = new Date();
+    return date.getFullYear();
+  }
+  
+  export default function getBudgetForCurrentYear(income, gdp, capita) {
+    const key = getCurrentYear();
+    const budget = {
+  
+    [`income-${key}`]: income;
+    [`gdp-${key}`]: gdp;
+    [`capita-${key}`]: capita;
+    };
+  
+    return budget;
+  }
