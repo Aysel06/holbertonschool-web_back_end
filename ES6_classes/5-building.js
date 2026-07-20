d00cddb825d074ef7455b75bbae5db6c0599f3ff
Aysel06 <mammedovaaysel0@gmail.com>
2026-07-20 19:03:36 +0000

    ok i was new

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,16 @@
+export default class Building{
+    constructor(sqft){
+        this._sqft = sqft;
+
+        if(this.constructor !== Building && 
+            this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage){
+            throw new TypeError("Class extending Building must override evacuationWarningMessage")
+        }
+    }
+    evacuationWarningMessage(){}
+    
+
+    get sqft(){
+    return this._sqft ;
+}
+}
