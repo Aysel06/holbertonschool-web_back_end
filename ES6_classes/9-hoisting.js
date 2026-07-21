763877af06ed1cf9540f7ecf8ee258c4774ed02b
Aysel06 <mammedovaaysel0@gmail.com>
2026-07-21 19:12:15 +0000

    not bad

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,7 +1,7 @@
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-export default class HolbertonClass {
+export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
     this._location = location;
@@ -22,7 +22,7 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-export default class StudentHolberton {
+export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
