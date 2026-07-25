94e78f16f818665a34cacad93cbd2e359b81c9a6
Aysel06 <mammedovaaysel0@gmail.com>
2026-07-25 21:24:40 +0000

    1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+function startProgram() {
+  process.stdout.write('Welcome to Holberton School, what is your name?\n');
+  process.stdin.setEncoding('utf8');
+  process.stdin.on('data', function (data) {
+    process.stdout.write(`Your name is: ${data.trim()}\n`);
+  });
+  process.stdin.on('end', function () {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
+startProgram();
